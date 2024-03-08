e3b5365c914c48acf0e06149d235796e1baff8d0
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2024-03-08 16:44:59 +0300

    .

---
+++ index.js
@@ -0,0 +1,52 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 3000;
+// Initialize Google Generative AI with API key from environment variable
+const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+
+
+
+
+// POST /chat endpoint
+app.post('/chat', async (req, res) => {
+  try {
+    // Extract history and message from request body
+    const { history, message } = req.body;
+    console.log(history, message);
+    
+    // Define the model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    
+    // Start a chat session
+    const chat = model.startChat(history);
+    
+    // Send a message to the chat session
+    const result = await chat.sendMessage(message)
+    const response = result.response;
+    
+    // Log the response
+    console.log(result);
+    
+    // Send the response text to the client
+    res.send(response);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).send("Internal Server Error")
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
