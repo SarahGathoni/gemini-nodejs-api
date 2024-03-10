def90dbe45584c90b6a42867f75cf5595a5365d1
Sarah Gathoni <100662021+SarahGathoni@users.noreply.github.com>
2024-03-10 10:00:13 +0300

    Update index.js
    change port

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use('/frontend', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
